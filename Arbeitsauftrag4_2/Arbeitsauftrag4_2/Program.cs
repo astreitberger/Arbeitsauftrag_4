6bef497c6653ccdd662940eac49656009dc628d8
Anna Streitberger <astreitberger@schueler.hakzell.at>
2020-03-27 23:27:23 +0100

    Uebung 2 hinzugefuegt

---
+++ Arbeitsauftrag4_2/Arbeitsauftrag4_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbeitsauftrag4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // mit Hilfe von Kilian
+
+            try
+            {
+                int zahl1 = 10;
+                int zahl2 = 0;
+
+                int ergebnis = zahl1 / zahl2;
+
+                Console.WriteLine("Ergebnis: {0}", ergebnis);
+            }
+            catch (DivideByZeroException exeption)
+            {
+                Console.WriteLine(exeption.Message);
+            }
+        }
+    }
+}
