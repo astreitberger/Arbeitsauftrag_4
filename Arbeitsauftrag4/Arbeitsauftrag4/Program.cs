4b22ce7bd424072c11d1c0e6be0d752f4fab6283
Anna Streitberger <astreitberger@schueler.hakzell.at>
2020-03-27 23:26:48 +0100

    Uebung 1 hinzugefuegt

---
+++ Arbeitsauftrag4/Arbeitsauftrag4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbeitsauftrag4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // mit Hilfe von Kilian
+
+            string[] namen = new string[3];
+
+            int counter = 0;
+
+
+            try
+            {
+                foreach (string name in namen)
+                {
+                    counter++;
+                    Console.Write("{0} Name hinzufügen: ", counter);
+                    namen[0] = Console.ReadLine();
+                }
+
+                Console.Write("letzter Name: ");
+                namen[4] = Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Es ist ein Fehler aufgetreten.");
+            }
+            finally
+            {
+                Console.WriteLine("Das Programm wurde beendet.");
+            }
+        }
+    }
+}
